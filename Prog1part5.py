8da1e6af332b6e4fcfb5c456eb0a26a05be45d23
khiggi11 <klhigginson@gmail.com>
2026-08-31 22:52:10 -0500

    Create Prog1part5.py

---
+++ Prog1part5.py
@@ -0,0 +1,8 @@
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+address = input("Enter address: ")
+city = input("Enter city: ")
+state = input("Enter state: ")
+zip_code = input("Enter zip code: ")
+
+print(f"{first_name} {last_name}\n{address}\n{city}, {state} {zip_code}")
