af69434891baec812eafeca2199b546fe732e357
khiggi11 <klhigginson@gmail.com>
2026-08-31 22:41:24 -0500

    Create Prog1part1.py

---
+++ Prog1part1.py
@@ -0,0 +1,10 @@
+# name:kailei Higginson
+# email:kailei.higginson@und.edu
+ 
+hours = int(input("Enter hours: "))
+minutes = int(input("Enter minutes: "))
+seconds = int(input("Enter seconds: "))
+
+total_seconds = hours * 3600 + minutes * 60 + seconds
+
+print(total_seconds, "seconds")
