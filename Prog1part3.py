9c10b2b0982a83ae5b84d401b6d24a95b3b0db17
khiggi11 <klhigginson@gmail.com>
2026-08-31 22:48:52 -0500

    Create Prog1part3.py

---
+++ Prog1part3.py
@@ -0,0 +1,8 @@
+twenties = int(input("Enter twenties: "))
+tens = int(input("Enter tens: "))
+fives = int(input("Enter fives: "))
+ones = int(input("Enter ones: "))
+
+total_dollars = twenties * 20 + tens * 10 + fives * 5 + ones
+
+print(f"${total_dollars}.00")
